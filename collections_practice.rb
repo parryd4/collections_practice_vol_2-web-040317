5e3b133b06f8649c3d015e3eeec4aac31a57fe87
Dan Parry <parryd424@gmail.com>
2017-07-26 11:40:55 -0400

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,21 @@
 # your code goes here
+def begins_with_r(arr)
+  arr.reject! {|word| word[0] == "r"}
+  arr.empty?
+end
+
+def contain_a(arr)
+  arr.select {|word| word.match(/[a]/)}
+end
+
+def first_wa(arr)
+  arr.find {|w| w.match(/\Awa/)}
+end
+
+def remove_non_strings(arr)
+  arr.select {|w| w.class == String}
+end
+
+def count_elements(arr)
+  
+end
